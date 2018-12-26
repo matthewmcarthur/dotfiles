d4ecaa727de5835a44cbee03b9710b3e6c08f55a
Matthew McArthur <matthew.mcarthur91@gmail.com>
2018-12-26 11:36:40 -0400

    enforce naming convention

--- elecom-scroll.sh
+++ elecom-scroll.sh
@@ -1,3 +1,4 @@
+# Set button Fn3 to scroll mode
 xinput set-prop "pointer:ELECOM TrackBall Mouse HUGE TrackBall" 'libinput Button Scrolling Button' 12
 xinput set-prop "pointer:ELECOM TrackBall Mouse HUGE TrackBall" 'libinput Scroll Method Enabled' 0 0 1
 
